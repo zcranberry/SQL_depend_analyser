18fead8976eacd1f1132095071bdb9c796ee3d6d
zcranberry <zxdpku@126.com>
2015-10-31 23:26:58 +0800

    findall改成finditer，好用很多

--- sql_parser.py
+++ sql_parser.py
@@ -3,9 +3,9 @@ import re
 from comment import comment_filter
 
 #需要筛选出来的关键词，以schema名字为特征
-keyword_pattern = re.compile(r'(\b(lsc|jcc|zbc|ywc|yyc)\.\w+\b)', flags=re.IGNORECASE)
+keyword_pattern = re.compile(r'\b(lsc|jcc|zbc|ywc|yyc)\.\w+\b', flags=re.IGNORECASE)
 #筛选出'from'关键字，以后可能会补充 as 等其他关键字
-from_pattern = re.compile(r'from', flags=re.IGNORECASE)
+from_pattern = re.compile(r'from|source_table', flags=re.IGNORECASE)
 
 class analyze_file:
     def __init__(self, input_file):
@@ -15,18 +15,20 @@ class analyze_file:
         self.lines = comment_filter(input_file.read())
 
     def source_target_finder(self, line):
-        keywords = re.findall(keyword_pattern, line)
+        keywords = re.finditer(keyword_pattern, line)
         from_word = re.search(from_pattern, line)
         if keywords and from_word:
             for keyword in keywords:
-                if line.find(keyword[0]) > line.find(from_word.group()[0]):
-                    self.source.add(keyword[0])
+                if  keyword.start() > from_word.start():
+                    self.source.add(keyword.group())
                 else:
-                    self.target.add(keyword[0])
+                    self.target.add(keyword.group())
 
     def process_file(self):
         for line in self.lines:
             self.source_target_finder(line)
+        #所有行处理完了之后，去除依赖自身的source,比如dsrzt_khjbxx
+        self.source -= self.target
 
     def print_result(self):
         print 'source:'
@@ -43,7 +45,7 @@ class analyze_file:
         
 
 if __name__ == '__main__':
-    f = open('KHFX_KHPJBQL_KHCYCPBQ_D.sql', 'r')
+    f = open('DSRZT_KHJBXXLSB_L.sql', 'r')
     clean_f = analyze_file(f)
     clean_f.demo()
 
