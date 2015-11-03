51a24fd5da565f7989f587866635ca574938d5d3
zcranberry <zxdpku@126.com>
2015-11-03 23:37:26 +0800

    logfile检查

--- config_xls_rd.py
+++ config_xls_rd.py
@@ -8,13 +8,19 @@ class job:
             self.sql_name = self.cmd_line.split(' ')[1].split('.')[0]
         except:
             self.sql_name = ''
+        self.estimate_logfile = r'/rdmetl/log/' + self.jobflow_name.split('_')[1].lower() + r'/%%yyyymmdd./'+self.sql_name + r'.sql.log'
 
     def __unicode__(self):
-        retstr = 'Jobflow_name: ' + self.jobflow_name +  '\nJob_name: ' + self.job_name + '\nCmd_line: ' + self.cmd_line + '\nsql_name: ' + self.sql_name + '\nComment: ' + self.comment + '\nLog: ' + self.log
+        retstr = 'Jobflow_name: ' + self.jobflow_name +  '\nJob_name: ' + self.job_name + '\nCmd_line: ' + self.cmd_line + '\nsql_name: ' + self.sql_name + '\nComment: ' + self.comment + '\nLog:  ' + self.log + '\nLog1: ' + self.estimate_logfile
         return retstr
 
     def __str__(self):
         return unicode(self).encode('gbk')
+
+
+    def consistency_check(self):
+        if self.estimate_logfile != self.log:
+            return False
 
 if __name__ == '__main__':
     data = xlrd.open_workbook('ctm_config.xls', encoding_override = 'cp1252')
@@ -28,7 +34,8 @@ if __name__ == '__main__':
 
     count = 0
     for each_job in jobs:
-        if each_job.jobflow_name + '_' + each_job.sql_name != each_job.job_name:
+        #if each_job.jobflow_name + '_' + each_job.sql_name != each_job.job_name:
+        if each_job.consistency_check() == False:
             print count
             print each_job
             print ''
