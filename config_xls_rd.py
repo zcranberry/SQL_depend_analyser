15b890b2f5031f2d6a8800960c3e94926792bfb3
zcranberry <zxdpku@126.com>
2015-11-05 00:06:29 +0800

    过滤多余空格

--- config_xls_rd.py
+++ config_xls_rd.py
@@ -1,11 +1,14 @@
 #encoding:utf-8
+# 期望功能：深度搜索任务依赖，验证文件依赖，日志路径，作业依赖
 import xlrd
+import re
 
 class job:
     def __init__(self, line):
         self.jobflow_name, self.job_name, self.cmd_line, self.freq, self.comment, self.log = line[0], line[1], line[3], line[5], line[14], line[15]
+        tmpline = re.sub(r'\s+', ' ', self.cmd_line) #去除多余空格
         try:
-            self.sql_name = self.cmd_line.split(' ')[1].split('.')[0]
+            self.sql_name = tmpline.split(' ')[1].split('.')[0]
         except:
             self.sql_name = ''
         self.estimate_logfile = r'/rdmetl/log/' + self.jobflow_name.split('_')[1].lower() + r'/%%yyyymmdd./'+self.sql_name + r'.sql.log'
@@ -19,23 +22,24 @@ class job:
 
 
     def consistency_check(self):
-        if self.estimate_logfile != self.log:
-            return False
+        if self.jobflow_name + '_' + self.sql_name.upper() != self.job_name:
+            return 'JobNameError'
+        #if self.estimate_logfile != self.log: #log配错目前较多，暂时不管
+        #    return 'LogFileError' 
+        return True
 
 if __name__ == '__main__':
-    data = xlrd.open_workbook('ctm_config.xls', encoding_override = 'cp1252')
+    data = xlrd.open_workbook('RDM_AFTER_20151324.xls', encoding_override = 'cp1252')
     names = data.sheet_names()
     table = data.sheets()[2] #Job Sheet
     
     jobs = []
-    for i in xrange(1, 955):
+    for i in xrange(1, table.nrows - 1):
         jobs.append(job(table.row_values(i)))
-    
 
     count = 0
     for each_job in jobs:
-        #if each_job.jobflow_name + '_' + each_job.sql_name != each_job.job_name:
-        if each_job.consistency_check() == False:
+        if each_job.consistency_check() != True :
             print count
             print each_job
             print ''
