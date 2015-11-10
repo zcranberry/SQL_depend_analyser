23658b6c8414ee0aeff097b49814bc8c0ec389d5
zcranberry <zxdpku@126.com>
2015-11-10 23:36:22 +0800

    将ctm依赖关联结构化

--- config_xls_rd.py
+++ config_xls_rd.py
@@ -37,32 +37,95 @@ class job_collection:
         self.job_set[name] = value
 
 
+
 #class job_visited:
 #    def __init__(self, 
+
+#####################################################################################################################################
+
+class job_dependence:#一条边
+    def __init__(self, line):
+        self.edge_start, self.edge_end = line
+
+class node:#一个节点
+    def __init__(self, name):
+        self.name = name
+        self.src = set()
+        self.target = set()
+    
+    def add_src(self, src):
+        self.src.add(src)
+
+    def add_target(self, target):
+        self.target.add(target)
+
+    def demo(self):
+        print 'Name: ' + self.name
+        print 'Src: '
+        for each in self.src:
+            print each
+        print 'target:'
+        for each in self.target:
+            print each
+
+        print ''
+class node_collections:
+    def __init__(self):
+        self.nodes= dict()
+    #def add_node(self, node):
+    #    self.nodes[node] = node()
+
+
+class job_dependece_graph:
+    def __init__(self):
+        self.graph_edges = set()
+        self.graph_nodes = node_collections()
+
+    def add_edge(self, edge):
+        #self.graph_edges.add(edge)
+        target, src = edge[0], edge[1]
+        if target not in self.graph_nodes.nodes:
+            self.graph_nodes.nodes[target] = node(target)
+
+        if src not in self.graph_nodes.nodes:
+            self.graph_nodes.nodes[src] = node(src)
+
+        self.graph_nodes.nodes[src].add_target(target) # 用节点还是名字待定
+        self.graph_nodes.nodes[target].add_src(src)
+
+
 
 
 
 if __name__ == '__main__':
     data = xlrd.open_workbook('RDM_AFTER_20151324.xls', encoding_override = 'cp1252')
-    names = data.sheet_names()
-    table = data.sheets()[2] #Job Sheet
-    
-    jobs = job_collection()
+    #names = data.sheet_names()
+    #table = data.sheets()[2] #Job Sheet
+    #
+    #jobs = job_collection()
 
-    #jobs = []
+
+    #for i in xrange(1, table.nrows - 1):
+    #    #jobs.append(job(table.row_values(i)))
+    #    j = job(table.row_values(i))
+    #    jobs.add(j.job_name, j)
+
+
+    #count = 0
+    #for each_job in jobs.job_set.values():
+    #    if each_job.consistency_check() != True :
+    #        print count
+    #        print each_job
+    #        print ''
+    #        count += 1
+    
+    table = data.sheets()[4]
+    graph = job_dependece_graph()
 
     for i in xrange(1, table.nrows - 1):
-        #jobs.append(job(table.row_values(i)))
-        j = job(table.row_values(i))
-        jobs.add(j.job_name, j)
+        graph.add_edge(table.row_values(i))
 
-
-    count = 0
-    for each_job in jobs.job_set.values():
-        if each_job.consistency_check() != True :
-            print count
-            print each_job
-            print ''
-            count += 1
+    for each in graph.graph_nodes.nodes:
+        graph.graph_nodes.nodes[each].demo()
 
     
